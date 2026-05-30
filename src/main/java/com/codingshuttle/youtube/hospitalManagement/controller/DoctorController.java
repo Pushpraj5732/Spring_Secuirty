d620340f277e21121b6186d46d55442fde142ee4
Pushpraj5732 <153991103+Pushpraj5732@users.noreply.github.com>
2026-05-30 18:36:54 +0530

    Change endpoint mapping from '/doctors' to '/doctor'

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/controller/DoctorController.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/controller/DoctorController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/doctors")
+@RequestMapping("/doctor")
 @RequiredArgsConstructor
 public class DoctorController {
 
